a4cd19e5fcc440cb40dd0f63aa89e753350dea7a
Nikhil-iitg27 <bhupathinikhil2006@gmail.com>
2025-01-01 17:43:18 +0530

    Made changes to create.py in modules

--- modules/create.py
+++ modules/create.py
@@ -14,17 +14,17 @@ def create_folder_structure(dir, courses, sem):
     sem_folder = os.path.join(dir['base_dir'], semester)
     os.makedirs(sem_folder, exist_ok=True)
     
-    # Create Pre-Mid and Post-Mid folders
-    pre_mid_folder = os.path.join(sem_folder, "Pre-Mid")
-    post_mid_folder = os.path.join(sem_folder, "Post-Mid")
-    os.makedirs(pre_mid_folder, exist_ok=True)
-    os.makedirs(post_mid_folder, exist_ok=True)
-    
     # Create course folders
     for course in courses:
-        os.makedirs(os.path.join(pre_mid_folder, course), exist_ok=True)
-        os.makedirs(os.path.join(post_mid_folder, course), exist_ok=True)
-
+        course_folder = os.path.join(sem_folder, course)
+        os.makedirs(course_folder, exist_ok=True)
+        os.makedirs(course_folder, exist_ok=True)
+        # Create Pre-Mid and Post-Mid folders
+        pre_mid_folder = os.path.join(course_folder, "Pre-Mid")
+        post_mid_folder = os.path.join(course_folder, "Post-Mid")
+        os.makedirs(pre_mid_folder, exist_ok=True)
+        os.makedirs(post_mid_folder, exist_ok=True)
+    
     print(f"Folder structure for {sem} created successfully.")
     time.sleep(0.2)
 
